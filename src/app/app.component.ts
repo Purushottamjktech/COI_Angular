690924b5935582b8dbd923745367d4419b1aed98
Purushottam.kumar <purushottam.kumar@jktech.com>
2022-06-23 13:46:11 +0530

    Observables Implemented.....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-loadedFeature ='Recipe';
 
-
-  onNavigator(feature:string){
-this.loadedFeature=feature;
-  }
 }
